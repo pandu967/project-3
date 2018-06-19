ddc50672d53a204deb1189abd0a5f8c8f5e0452f
Tejaswi967 <38848734+pandu967@users.noreply.github.com>
2018-06-19 21:05:40 +0530

    Update logorig.py

--- logorig.py
+++ logorig.py
@@ -1,113 +1,68 @@
-#!/usr/bin/env python
+#!usr/bin/env python3
 import psycopg2
-
-DBNAME = "news"
-
-
-def run_query(query):
-    """Connects to the database, runs the query passed to it,
-    and returns the results"""
-    db = psycopg2.connect('dbname=' + DBNAME)
-    cur = db.cursor()
-    cur.execute(query)
-    rows = cur.fetchall()
-    db.close()
-    return rows
-
-
-class log:
-
-    def get_top_articles(self):
-        """Returns top 3 most read articles"""
-
-        # Build Query String
-        query = """
-            SELECT articles.title, COUNT(*) AS num
-            FROM articles
-            JOIN log
-            ON log.path LIKE concat('/article/%', articles.slug)
-            GROUP BY articles.title
-            ORDER BY num DESC
-            LIMIT 3;
-        """
-
-        # Run Query
-        results = run_query(query)
-
-        # Print Results
-        print('\n          TOP THREE ARTICLES BASED ON VIEWS')
-        count = 1
-        for i in results:
-            number = str(count) + '. "'
-            title = i[0]
-            views = '" with ' + str(i[1]) + " views"
-            print(number + title + views)
-            count += 1
+articles = """select articles.titles,
+           count(*) as n from log,articles
+           where log.status="%200%"
+           group by articles.title
+           order by n desc limit 3"""
+conn = psycopg2.connect(dbname='news')
+cur = conn.cursor()
+cur.execute(articles)
+print("     TOP THREE ARTICLES")
+print("Title                                       Views")
+c = cur.fetchall()
+for title, n in c:
+    print(title, n)
+cur.close()
+conn.close()
 
 
-def get_top_article_authors(self):
-        """returns top 3 most popular authors"""
+authors = """select authors.name, count(*)as n from articles,
+           authors, log where log.status="%200%" and
+           authors.id=articles.author
+           group by authors.name
+           order by n desc"""
+conn = psycopg2.connect(dbname='news')
+cur = cunn.cursor()
+cur.execute(authors)
+print("       POPULAR AUTHORS")
+print("Author                       Views")
+c = cur.fetchall()
+for a, n in c:
+    print(a, n)
+cur.close()
+conn.close()
 
-        # Build Query String
-        query = """
-            SELECT authors.name, COUNT(*) AS num
-            FROM authors
-            JOIN articles
-            ON authors.id = articles.author
-            JOIN log
-            ON log.path like concat('/article/%', articles.slug)
-            GROUP BY authors.name
-            ORDER BY num DESC
-            LIMIT 3;
-        """
-
-        # Run Query
-        results = run_query(query)
-
-        # Print Results
-        print('\n          TOP THREE AUHOTS BASED ON VIEWS:')
-        print('')
-        count = 1
-        for i in results:
-            print(str(count) + '. ' + i[0] + ' with ' + str(i[1]) + " views")
-            count += 1
-
-
-def get_days_with_errors(self):
-        """returns days with more than 1% errors"""
-
-        # Build Query String
-        query = """
-            SELECT total.day,
-              ROUND(((errors.error_requests*1.0) / total.r"
-              "equests), 3) AS percent
-            FROM (
-              SELECT date_trunc('day', time) "day", count(*) AS error_requests
-              FROM log
-              WHERE status LIKE '404%'
-              GROUP BY day
-            ) AS errors
-            JOIN (
-              SELECT date_trunc('day', time) "day", count(*) AS requests
-              FROM log
-              GROUP BY day
-              ) AS total
-            ON total.day = errors.day
-            WHERE (ROUND(((errors.error_requests*1.0) / total.reque"
-            "sts), 3) > 0.01)
-            ORDER BY percent DESC;
-        """
-
-        # Run Query
-        results = run_query(query)
-
-        # Print Results
-        print('\n          DAYS WITH MAORE THAN 1% ERROR:')
-        for i in results:
-            date = i[0].strftime('%B %d, %Y')
-            errors = str(round(i[1]*100, 1)) + "%" + " errors"
-            print(date + " -- " + errors)
-a = log()
-a.get_top_articles()
-a.get_top_article_authors()
-a.get_days_with_errors()
+conn = psycopg2.connect(dbname='news')
+cur = cunn.cursor()
+t_req = """select count(*) as c,
+        date(TIME) as date
+        from log group by date order by c desc"""
+e_req = """select count(*) as c,
+        date(TIME) as date
+        from log
+        where log.status="%200%"
+        group by date
+        order by c desc"""
+e_perc = """select  t_req.date,
+        round((100.0*e_req.count)/t_req.count,2)
+        as e_per from e_req,
+        t_req where e_req=t_req"""
+error = """select to_char(date, 'Mon DD YYYY')
+        as date, e_per
+        from e_perc where e_per>1.0"""
+try:
+    cur.execute(t_req)
+    cur.execute(e_req)
+    cur.execute(e_perc)
+except Exception as e:
+    print(e)
+c = cur.execute(error)
+res = c.fetchall()
+for i in range(len(res)):
+    d = res[i][0]
+    e = res[i][1]
+    print("more number of errors were "
+          "encountered on "d " with " e " percentage")
+cur.close()
+conn.close()
