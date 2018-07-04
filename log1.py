921f456f54dfa9cc99c71b1e75eec7086a2af247
Tejaswi967 <38848734+pandu967@users.noreply.github.com>
2018-07-04 11:42:56 +0530

    Update log1.py

--- log1.py
+++ log1.py
@@ -1,20 +1,21 @@
 #!usr/bin/env python3
 import psycopg2
-'''article = """select title,count(title) as views
-from articles join log on log.path like concat('%',articles.slug,'%')
-where log.status like '%200%'
-group by articles.title order by views desc limit 3;
-"""
+article = """select title,count(title) as views
+           from articles
+           join log on log.path
+           like concat('%',articles.slug,'%')
+           where log.status like '%200%'
+           group by articles.title order by views desc limit 3"""
 conn = psycopg2.connect(dbname='news')
 cur = conn.cursor()
 cur.execute(article)
 print("     TOP THREE ARTICLES")
 print("Title                                       Views")
-results=cur.fetchall()
+results = cur.fetchall()
 for i in range(len(results)):
-    title=results[i][0]
-    views=results[i][1]
-    print("%s--%d" % (title,views))
+    title = results[i][0]
+    views = results[i][1]
+    print("%s--%d" % (title, views))
 cur.close()
 conn.close()
 
@@ -36,34 +37,36 @@ for a, n in c:
     print(a, n)
 cur.close()
 conn.close()
-'''
 conn = psycopg2.connect(dbname='news')
 cur = conn.cursor()
-total_request = """select count(*) as count,
-        date(TIME) as date
-        from log group by date order by count desc"""
-error_request = """select count(*) as count,
-        date(TIME) as date
-        from log
-        where log.status!='%200%'
-        group by date
-        order by count desc"""
-err_perc = """select  total_request.date,
-        round((100.0*error_request.count)/total_request.count,2) as err_prc
-        from error_request,
-        total_request where error_request.date=total_request.date"""
-error = """select to_char(date, 'Mon DD YYYY')
-        as date, err_prc
-        from err_prc where err_prc>1.0"""
-cur.execute(total_request)
-cur.execute(error_request)
-cur.execute(err_perc)
-c = cur.execute(error)
-res = c.fetchall()
-for i in range(len(res)):
-    d = res[i][0]
-    e = res[i][1]
-    print("more number of errors were "
-          "encountered on "+d+ " with " +e+ " percentage")
+
+# Build Query String
+query = """
+        SELECT total.day,
+          ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent
+        FROM (
+          SELECT date_trunc('day', time) "day", count(*) AS error_requests
+          FROM log
+          WHERE status LIKE '404%'
+          GROUP BY day
+        ) AS errors
+        JOIN (
+          SELECT date_trunc('day', time) "day", count(*) AS requests
+          FROM log
+          GROUP BY day
+          ) AS total
+        ON total.day = errors.day
+        WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01)
+        ORDER BY percent DESC;
+    """
+conn = psycopg2.connect(dbname='news')
+cur = conn.cursor()
+cur.execute(query)
+res = cur.fetchall()
+print('\n more number of errors were encountered on:')
+for i in re:
+    d = i[0].strftime('%B %d, %Y')
+    e = str(round(i[1]*100, 1)) + "%" + " errors"
+    print(d + " with " + e)
 cur.close()
 conn.close()
